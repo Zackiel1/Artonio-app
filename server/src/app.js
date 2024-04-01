2a4f639ff05e8b8a77ff9336aafd43950061d88c
Zackiel1 <alejandrozfm132@gmail.com>
2024-04-01 20:15:53 -0300

    probando soluconar error de cors

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,10 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
