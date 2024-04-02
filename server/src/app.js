2e16830c24bf93fc0babbdeea7857a50b4182276
Zackiel1 <alejandrozfm132@gmail.com>
2024-04-02 10:40:29 -0300

    veamos 2.0

--- server/src/app.js
+++ server/src/app.js
@@ -14,40 +14,32 @@ const app = express();
 
 app.use(cors());
 app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
-      "Access-Control-Allow-Origin",
-      "http://localhost:3000/"
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
     );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE',
+    );
     next();
-  });
+});
+
 app.use(morgan('dev'));
 app.use(express.json());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept',
-//     );
-//     res.header(
-//         'Access-Control-Allow-Methods',
-//         'GET, POST, OPTIONS, PUT, DELETE',
-//     );
-//     next();
-// });
 app.use(passport.initialize());
 
 app.use(mainRouter);
 
-// // Error catching endware.
-// app.use((err, req, res, next) => {
-//   // eslint-disable-line no-unused-vars
-//   const status = err.status || 500;
-//   const message = err.message || err;
-//   console.error(err);
-//   res.status(status).send(message);
-// });
+// Error catching endware.
+app.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = app;
