07deabe0aa317f9799799baeb69aaf6ca276f294
Zackiel1 <alejandrozfm132@gmail.com>
2024-04-02 10:33:02 -0300

    veamos

--- server/src/app.js
+++ server/src/app.js
@@ -12,18 +12,18 @@ require("../src/services/google.js")
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
     res.header(
       "Access-Control-Allow-Origin",
-      "React app URL"
+      "http://localhost:3000/"
     );
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
   });
+app.use(morgan('dev'));
+app.use(express.json());
 // app.use((req, res, next) => {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Credentials', 'true');
