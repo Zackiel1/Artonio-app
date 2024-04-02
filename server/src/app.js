c128c60fa4f37611c1f6999b07689ec80c00f8a9
Zackiel1 <alejandrozfm132@gmail.com>
2024-04-02 11:37:49 -0300

    funciona en mzilla pero no en chrome

--- server/src/app.js
+++ server/src/app.js
@@ -12,20 +12,20 @@ require("../src/services/google.js")
 
 const app = express();
 
-// app.use(cors());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.header(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept',
-//     );
-//     res.header(
-//         'Access-Control-Allow-Methods',
-//         'GET, POST, OPTIONS, PUT, DELETE',
-//     );
-//     next();
-// });
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE',
+    );
+    next();
+});
 
 app.use(morgan('dev'));
 app.use(express.json());
